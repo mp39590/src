e6e0342912f74f45626c7319e8d09228745ae4d6
Mikhail <mp39590@gmail.com>
2025-01-09 18:34:24 +0300

    init_h2c

--- sys/dev/usb/if_urtwm.c
+++ sys/dev/usb/if_urtwm.c
@@ -7641,6 +7641,59 @@ int rtw_core_init(struct rtw_dev *rtwdev)
 
 // {{{ power_on
 
+static int init_h2c(struct rtw_dev *rtwdev)
+{
+        struct rtw_fifo_conf *fifo = &rtwdev->fifo;
+        u8 value8;
+        u32 value32;
+        u32 h2cq_addr;
+        u32 h2cq_size;
+        u32 h2cq_free;
+        u32 wp, rp;
+
+        if (rtw_chip_wcpu_11n(rtwdev))
+                return 0;
+
+        h2cq_addr = fifo->rsvd_h2cq_addr << TX_PAGE_SIZE_SHIFT;
+        h2cq_size = RSVD_PG_H2CQ_NUM << TX_PAGE_SIZE_SHIFT;
+
+        value32 = rtw_read32(rtwdev, REG_H2C_HEAD);
+        value32 = (value32 & 0xFFFC0000) | h2cq_addr;
+        rtw_write32(rtwdev, REG_H2C_HEAD, value32);
+
+        value32 = rtw_read32(rtwdev, REG_H2C_READ_ADDR);
+        value32 = (value32 & 0xFFFC0000) | h2cq_addr;
+        rtw_write32(rtwdev, REG_H2C_READ_ADDR, value32);
+
+        value32 = rtw_read32(rtwdev, REG_H2C_TAIL);
+        value32 &= 0xFFFC0000;
+        value32 |= (h2cq_addr + h2cq_size);
+        rtw_write32(rtwdev, REG_H2C_TAIL, value32);
+
+        value8 = rtw_read8(rtwdev, REG_H2C_INFO);
+        value8 = (u8)((value8 & 0xFC) | 0x01);
+        rtw_write8(rtwdev, REG_H2C_INFO, value8);
+
+        value8 = rtw_read8(rtwdev, REG_H2C_INFO);
+        value8 = (u8)((value8 & 0xFB) | 0x04);
+        rtw_write8(rtwdev, REG_H2C_INFO, value8);
+
+        value8 = rtw_read8(rtwdev, REG_TXDMA_OFFSET_CHK + 1);
+        value8 = (u8)((value8 & 0x7f) | 0x80);
+        rtw_write8(rtwdev, REG_TXDMA_OFFSET_CHK + 1, value8);
+
+        wp = rtw_read32(rtwdev, REG_H2C_PKT_WRITEADDR) & 0x3FFFF;
+        rp = rtw_read32(rtwdev, REG_H2C_PKT_READADDR) & 0x3FFFF;
+        h2cq_free = wp >= rp ? h2cq_size - (wp - rp) : rp - wp;
+
+        if (h2cq_size != h2cq_free) {
+                printf("%s: H2C queue mismatch\n", __func__);
+                return -EINVAL;
+        }
+
+        return 0;
+}
+
 static int __priority_queue_cfg(struct rtw_dev *rtwdev,
                                 const struct rtw_page_table *pg_tbl,
                                 u16 pubq_num)
@@ -7840,10 +7893,10 @@ static int rtw_init_trx_cfg(struct rtw_dev *rtwdev)
 	ret = priority_queue_cfg(rtwdev);
 	if (ret)
 		return ret;
-//
-//        ret = init_h2c(rtwdev);
-//        if (ret)
-//                return ret;
+
+	ret = init_h2c(rtwdev);
+	if (ret)
+		return ret;
 
         return 0;
 }
